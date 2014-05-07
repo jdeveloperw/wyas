a6c24c853fe6905b16d3733ba7e21e7a1a323c36
JD White <jdeveloperw@gmail.com>
2014-05-07 10:43:48 -0700

    Working version of First Steps: Exercise 3; also clean up imports

--- 0-first-steps/hello_world.hs
+++ 0-first-steps/hello_world.hs
@@ -1,11 +1,9 @@
 module Main where
 import System.Environment
-import Data.List (elemIndex)
-import Data.Maybe (fromMaybe)
-import Data.String (String)
 
  
 main :: IO ()
 main = do
-   args <- getArgs
-   putStrLn $ show $ foldl (\acc x -> acc + x) 0 $ map read args
+   putStrLn "Enter name: "
+   input <- getLine
+   putStrLn $ "Hello, " ++ input
