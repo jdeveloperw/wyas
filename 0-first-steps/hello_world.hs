196b80b00f00bb52070d4b603a39b2691b7e412c
JD White <jdeveloperw@gmail.com>
2014-05-06 22:59:15 -0700

    Working version of First Steps: Exercise 1

--- 0-first-steps/hello_world.hs
+++ 0-first-steps/hello_world.hs
@@ -1,7 +1,17 @@
- module Main where
- import System.Environment
+module Main where
+import System.Environment
+import Data.List (elemIndex)
+import Data.Maybe (fromMaybe)
+import Data.String (String)
+
  
- main :: IO ()
- main = do
-     args <- getArgs
-     putStrLn ("Hello, " ++ args !! 0)
+main :: IO ()
+main = do
+   args <- getArgs
+   putStrLn ("Hello, " ++ (elemOrEmpty 0 args) ++ " " ++ (elemOrEmpty 1 args))
+
+
+elemOrEmpty :: Int -> [String] -> String
+elemOrEmpty i xs
+  | i < length xs = xs !! i
+  | otherwise     = ""
