15ab8d53a154d7bc7e66b2f955d7fe21b5c92aba
JD White <jdeveloperw@gmail.com>
2014-05-07 11:19:13 -0700

    Working version of Parsing: Example 3 -- use LispVal

--- 1-Parsing/parser.hs
+++ 1-Parsing/parser.hs
@@ -1,19 +1,49 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
 
-symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
-
-spaces :: Parser ()
-spaces = skipMany1 space
-
-readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
-   Left err -> "No match: " ++ show err
-   Right val -> "Found value"
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
 
 main :: IO ()
 main = do 
        args <- getArgs
        putStrLn (readExpr (args !! 0))
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+   Left err -> "No match: " ++ show err
+   Right val -> "Found value"
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+               first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest
+               return $ case atom of 
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          _    -> Atom atom
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseString :: Parser LispVal
+parseString = do
+                 char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
